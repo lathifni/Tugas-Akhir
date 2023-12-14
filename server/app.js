c3aab8d04faef4571f435471833753304bae6a1e
Lathif Nur Irsyad <lathifni.ni@gmail.com>
2023-12-14 18:57:59 +0700

    Google auth sederhana

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var googleRuter = require('./routes/oauthGoogle')
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', googleRuter)
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ server/routes/oauthGoogle.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const OAuth2 = require('../controllers/oauthGoogle')
+
+var router = express.Router();
+
+router.get('/auth/google/', OAuth2.Login)
+
+router.get('/auth/google/callback', OAuth2.CallBackLogin)
+
+module.exports = router
